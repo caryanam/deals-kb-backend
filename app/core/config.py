1752fd515dc699377a22c8f46bbd7c1aa4c1518f
caryanamPratikp <pratikpatilcaryanamindia.in>
2026-07-09 17:23:16 +0530

    cors updt

--- app/core/config.py
+++ app/core/config.py
@@ -20,7 +20,7 @@ class Settings:
         "http://localhost:3000",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
-        "https://d1.dealskb.com",
+        "https://backend.dealskb.com",
         "https://dealskb.com",
         "https://www.dealskb.com",
     ]
